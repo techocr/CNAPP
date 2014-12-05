c12f3c38aff23619fea1dadbebcc4c9da20eda99
techocr <logistica.costarica@techo.org>
2014-12-05 17:54:35 -0600

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CNAPP::Application.config.secret_token = '8877eb380ee51248e24441ad414dbf793e17c6fd7ac4ac3c4e0d59553250c1cb9cb3049e0b70444058f2dd6b09a86885f6b1cf66e3a24665be7904c490abdf3e'
